ee026f5dba910b4f93d2bc65d2665a8174cc24e3
Subodh20482 <149533665+Subodh20482@users.noreply.github.com>
2025-01-25 17:09:48 +0530

    login user

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -12,6 +12,14 @@ router.post('/register', [
 ) 
 
 
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    ],
+    userController.loginUser
+)
+
     
 
 
