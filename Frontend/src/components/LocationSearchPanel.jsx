254ae6f25319ad1c8c9a1fc4d93fedbeda7b95fd
suborazz <subodh7488770476@gmail.com>
2025-01-30 13:00:19 +0530

    locationg

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const LocationSearchPanel = () => {
-  return (
-    <div>LocationSearchPanel</div>
-  )
-}
+  //sample array for location
+  const location = [
 
-export default LocationSearchPanel
+
+
+    
+  ];
+
+  return (
+    <div>
+      {/* This is the temp Location */}
+      {/* First Location */}
+      <div className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-8 rounded-full">
+          <i className="ri-map-pin-fill text" />
+        </h2>
+        <h4 className="font-medium">
+          Chandhigarh University <p>NH-05, Ludhiana, Higway, Chandhigarh</p>
+          State, Punjab 140413
+        </h4>
+      </div>
+
+      {/* Second Location */}
+      <div className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-4 justify-start mt-4">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-8 rounded-full">
+          <i className="ri-map-pin-fill text" />
+        </h2>
+        <h4 className="font-medium">
+          Amity University <p>Sector-125, Noida, Uttar Pradesh</p>
+          State, 201313
+        </h4>
+      </div>
+
+      {/* First Location */}
+      <div className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-8 rounded-full">
+          <i className="ri-map-pin-fill text" />
+        </h2>
+        <h4 className="font-medium">
+          Chandhigarh University <p>NH-05, Ludhiana, Higway, Chandhigarh</p>
+          State, Punjab 140413
+        </h4>
+      </div>
+
+      {/* Second Location */}
+      <div className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-4 justify-start mt-4">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-8 rounded-full">
+          <i className="ri-map-pin-fill text" />
+        </h2>
+        <h4 className="font-medium">
+          Amity University <p>Sector-125, Noida, Uttar Pradesh</p>
+          State, 201313
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
   );
 
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       {/* Logo */}
       <img
         className="w-16 absolute top-5 left-5"
@@ -54,8 +54,8 @@ const Home = () => {
       <div className=" w-screen h-screen">
         <img className="h-full w-full  object-cover" src={map} alt="map" />
       </div>
-      <div className="flex flex-col justify-end h-full  right-0 top-0 left-0 W-full  absolute">
-        <div className="relative h-[30%]  bg-white ">
+      <div className="flex flex-col justify-end h-full w-full absolute bottom-0">
+        <div className="relative h-[30%]  bg-white p-5">
           <h5
             ref={panelCloseRef}
             onClick={() => setPanelOpen(false)}
@@ -69,7 +69,7 @@ const Home = () => {
             className="mt-4 flex flex-col space-y-2 px-5"
           >
             {/* Vertical Line Indicator */}
-            <div className="line absolute h-13 w-1 top-[35%] left-10 bg-gray-900 rounded-full"></div>
+            <div className="line absolute h-13 w-1 top-[45%] left-15 bg-gray-900 rounded-full"></div>
 
             {/* Pickup Input */}
             <input
@@ -95,6 +95,61 @@ const Home = () => {
         //{" "}
         <div ref={panelRef} className="bg-white h-0">
           <LocationSearchPanel />
+        </div>
+      </div>
+
+      <div className="fixed z-10 bottom-0 translate-y-full bg-white px-3 py-8 w-full">
+        <h3 className="text-2xl font-semibold mb-5">Choose a vericle</h3>
+        <div className="flex border-2 border-gray-50 active:border-black mb-2 rounded-xl  w-full p-3 items-center justify-between">
+          <img
+            className="h-12"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_715,w_1072/v1548646935/assets/64/93c255-87c8-4e2e-9429-cf709bf1b838/original/3.png"
+          ></img>
+          <div className="-ml-2 w-1/2">
+            <h4 className="font-medium text-base">
+              SwiftGo{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>4
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">2 mins away</h5>
+            <p className="font-normal text-xs">Affordable, compact rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹143.32</h2>
+        </div>
+        <div className="flex border-2 border-gray-50 active:border-black mb-2 rounded-xl  w-full p-3 items-center justify-between">
+          <img
+            className="h-10"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+          ></img>
+          <div className="-ml-2 w-1/2">
+            <h4 className="font-medium text-base">
+              Moto{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>1
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">3 mins away</h5>
+            <p className="font-normal text-xs">Affordable, motocycle rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹59</h2>
+        </div>
+        <div className="flex border-2 border-gray-50 active:border-black mb-2 rounded-xl  w-full p-3 items-center justify-between">
+          <img
+            className="h-10"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+          ></img>
+          <div className="-ml-2 w-1/2">
+            <h4 className="font-medium text-base">
+              SwiftAuto{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>3
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">3 mins away</h5>
+            <p className="font-normal text-xs">Affordable, auto rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹115</h2>
         </div>
       </div>
     </div>
